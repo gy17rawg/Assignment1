a0353fe55e4cb6dd3fae3c12a6afe57662c1c3f4
gy17rawg <gy17rawg@leeds.ac.uk>
2022-04-27 21:20:17 +0100

    Comments

--- agentframework.py
+++ agentframework.py
@@ -3,86 +3,88 @@
 """
 Created on Wed Feb  9 15:45:03 2022
 
+'''This framework enables the creation of agents and stores their attributes. It also contains the functions
+for eating the environment, moving and interacting'''
+
 @author: rorygrindey
 """
 
 import random
 
-#Basic form of a class and constructor
+# Basic form of a class and constructor
 
 class Agent():
-    def __init__(self, environment,agents, y, x): #Creation of agents, bringing in environment list
-        
-        if (x==None): #Only assigns a random x if the agent hasn't been assigned one from the online list (if there are more agents than values)
-                self.x = random.randint(0,299) #Assigns a random x between 0 and 299 (This uses the entire environment)
-                print("Random Used x")
-                
+    def __init__(self, environment, agents, y, x):  # Creation of agents, bringing in environment list
+
+        if x is None:  # Only assigns a random x if the agent hasn't been assigned one from the online list (if there are more agents than values)
+            self.x = random.randint(0, 299)  # Assigns a random x between 0 and 299 (This uses the entire environment)
+            print("Random Used x")
+
         else:
-                
+
             self.x = x
-        
-        if (y==None): #Only assigns a random y if the agent hasn't been assigned one from the online list
-                self.y = random.randint(0,299) #Assigns a random y between 0 and 299 (This uses the entire environment)
-                print("Random Used y")
-                
+
+        if y is None:  # Only assigns a random y if the agent hasn't been assigned one from the online list
+            self.y = random.randint(0, 299)  # Assigns a random y between 0 and 299 (This uses the entire environment)
+            print("Random Used y")
+
         else:
-                
+
             self.y = y
-            
-        self.environment=environment #means when the environment changes, it changes for all agents as its a mutable list
-        self.agents=agents
-        self.store=0
-        
-        
+
+        self.environment = environment  # means when the environment changes, it changes for all agents as its a mutable list
+        self.agents = agents
+        self.store = 0  # All start with a store of 0
+
     def move(self):
-        
+        # This function moves the agents/sheep by 1 in a direction dependant on a random number
         if random.random() < 0.5:
-            
-            self.y = (self.y + 1) % 300 #remainder means they move over the plot like a donut
-            
+
+            self.y = (self.y + 1) % 300  # remainder means they move over the plot like a donut (300 as this is the size of the environment)
+
         else:
-            
-            self.y = (self.y - 1) % 300 #Change 100 based on axis size
-            
-            
+
+            self.y = (self.y - 1) % 300  # Changed from 100 based on axis size
+
         if random.random() < 0.5:
-            
+
             self.x = (self.x + 1) % 300
-            
+
         else:
-            
+
             self.x = (self.x - 1) % 300
-            
-        
-        #matplotlib.pyplot.scatter(self.x,self.y)
-        #print(self.x,self.y)
 
+        # matplotlib.pyplot.scatter(self.x,self.y)
+        # print(self.x,self.y)
 
-    def eat(self): #eat what is left of the environment in each move step (sheep)
-        
-    #Eats what is left if less than 10
-        if self.environment[self.y][self.x]<10:
+    def eat(self):  # eat what is left of the environment in each move step (sheep)
+
+        # Eats what is left of the environment if less than 10
+        if self.environment[self.y][self.x] < 10:
             self.store += self.environment[self.y][self.x]
-            self.environment[self.y][self.x]-self.environment[self.y][self.x]
-                    
-        if self.environment[self.y][self.x]>10: #eats environment at its position if the value is greater than 10. Gets that location in the environment list
-            self.environment[self.y][self.x]-=10
+            self.environment[self.y][self.x] = 0
+
+        # If environment value is greater than 10, it only eats 10
+        if self.environment[self.y][self.x] > 10:  # eats environment at its position
+            self.environment[self.y][self.x] -= 10
             self.store += 10
-        
-        if self.store >100:
-            self.environment[self.y][self.x]+=self.store
-            self.store=0
-        
-    def share_with_neighbours(self, neighbourhood): #Method/function to search for close neighbours and share resources with them. Should work out distance to other agents
+
+        # If the store value is greater than 100, the agent/sheep throws it up back to add to the environment
+        if self.store > 100:
+            self.environment[self.y][self.x] += self.store
+            self.store = 0
+
+# Method/function to search for close neighbours and share resources with them. Should work out distance to other agents
+
+    def share_with_neighbours(self, neighbourhood):
         for agent in self.agents:
             distance = self.distance_between(agent)
-            if distance<=neighbourhood: #Only communicate if the sheep is within the neighbourhood set
-                sum = self.store+agent.store
-                ave=sum/2
-                self.store=ave
-                agent.store=ave
-                #print("Sharing | Distance: " + str(distance) + " Average: " + str(ave))
-                
-    def distance_between(self,agent): 
-        return (((self.x-agent.x)**2)+((self.y-agent.y)**2))**0.5       
-    
+            if distance <= neighbourhood:  # Only communicate if the sheep is within the neighbourhood set
+                sum = self.store + agent.store  # Combine stores of self and 3rd party agent
+                ave = sum / 2  # Get average of stores
+                self.store = ave  # Commit the average of the store to each agent
+                agent.store = ave
+                # print("Sharing | Distance: " + str(distance) + " Average: " + str(ave))
+
+    def distance_between(self, agent):
+        return (((self.x - agent.x) ** 2) + ((self.y - agent.y) ** 2)) ** 0.5  # Get euclidean distance between agents
